415dd863fefe60c13f214a963c2d61a89ab02521
JP <jsniezek@crowdskout.com>
2015-01-19 11:13:21 -0500

    Initial commit

---
+++ demo/main.js
@@ -0,0 +1,3 @@
+var app = angular.module('app', ['e50Editor']);
+app.controller('MainCtrl', function($scope) {
+});
---
+++ src/directives/e50-filter-bar.js
@@ -0,0 +1,15 @@
+// angular.module('e50Editor')
+// .directive('e50FilterBar', function() {
+//   return {
+//     restrict: 'EA',
+//     replace:true,
+//     priority: 1,
+//     transclude: true,
+//     // TODO: determine whether this should be terminal or not
+//     terminal: true,
+//     scope: {
+//       bar: "=filter"
+//     },
+//     templateUrl: 'views/components/e50-filter-bar.tpl.html'
+//   };
+// });
