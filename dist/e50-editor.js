a279a6054643a0ae7b445916bca600f523a05b72
JP <jsniezek@crowdskout.com>
2015-05-01 15:18:23 -0400

    Added iframe load event for firefox

--- dist/e50-editor.js
+++ dist/e50-editor.js
@@ -181,43 +181,53 @@ angular.module('E50Editor')
         // Get the newly appended iframe
         iframe = elm.find('iframe');
 
-        var contents = iframe.contents();
-        contents.find('head').append("<style>.ng-hide{display:none !important;}body{margin:0;padding:0;}*:focus{outline:none;}");
-        contents.find('head').append('<link href="'+E50EditorConfig.fontAwesome+'" rel="stylesheet"/>');
+        function initIframe() {
+            var contents = iframe.contents();
+            contents.find('head').append("<style>.ng-hide{display:none !important;}body{margin:0;padding:0;}*:focus{outline:none;}");
+            contents.find('head').append('<link href="'+E50EditorConfig.fontAwesome+'" rel="stylesheet"/>');
 
-        var body = contents.find('body');
-        body.css({margin: 0, padding: 0});
+            var body = contents.find('body');
+            body.css({margin: 0, padding: 0});
 
-        // Set the iframe for later, so we can use it in our other editor directives
-        E50Documents.set(scope.iframeId, iframe);
+            // Set the iframe for later, so we can use it in our other editor directives
+            E50Documents.set(scope.iframeId, iframe);
 
-        // Emit the iframe id and document, in case we want to build our buttons outside of the iframe
-        scope.$emit('e50Document', scope.iframeId, true, iframe);
+            // Emit the iframe id and document, in case we want to build our buttons outside of the iframe
+            scope.$emit('e50Document', scope.iframeId, true, iframe);
 
-        // Compile and append the e50-editor directive
-        var directive = [
-          '<div e50-editor',
-            'ng-model="html"',
-            'toggle="toggle"',
-            'buttons="buttons"',
-            'iframe-id="iframeId"',
-            'override="override"',
-            'image-saved="imageSaved"',
-            'aviary-options="aviaryOptions"',
-            'footer-tpl="footerTpl">',
-          'initial editable content</div>'
-        ].join(' ');
+            // Compile and append the e50-editor directive
+            var directive = [
+              '<div e50-editor',
+                'ng-model="html"',
+                'toggle="toggle"',
+                'buttons="buttons"',
+                'iframe-id="iframeId"',
+                'override="override"',
+                'image-saved="imageSaved"',
+                'aviary-options="aviaryOptions"',
+                'footer-tpl="footerTpl">',
+              'initial editable content</div>'
+            ].join(' ');
 
-        var directiveElm = $compile(directive)(scope);
-        body.append(directiveElm);
+            var directiveElm = $compile(directive)(scope);
+            body.append(directiveElm);
 
-        // This will resize the iframe's height to it's html height.
-        var html = angular.element(scope.html);
-        var images = html.find('img');
-        iframe.height(500);
-        images.on('load', function() {
-          //iframe.height(iframe.contents().find('html').height());
+            // This will resize the iframe's height to it's html height.
+            var html = angular.element(scope.html);
+            var images = html.find('img');
+            iframe.height(500);
+            images.on('load', function() {
+              //iframe.height(iframe.contents().find('html').height());
+            });
+        }
+
+        // Firefox fix
+        iframe.load(function () {
+            initIframe();
         });
+
+        initIframe();
+
       }
     };
   }]);
